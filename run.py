58ba505c9c50850096ca476cfe8f24607e8e12c7
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-11-06 13:51:15 +0000

    Materialize.css implemented

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-from flask import render_template
-from taskmanager import app, db
-from taskmanager.models import Category, Task
+import os
+from taskmanager import app
 
 
-@app.route("/")
-def home():
-    return render_template("base.html")
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
